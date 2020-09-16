a709cc8cec664c164b27936a1beca1b446dd026f
Deven Jahnke <DevenDoes@users.noreply.github.com>
2020-09-15 20:55:41 -0400

    A04 Complete

--- a04/render.js
+++ a04/render.js
@@ -1,7 +1,7 @@
 /**
  * Course: COMP 426
  * Assignment: a04
- * Author: <type your name here>
+ * Author: Deven Jahnke
  *
  * This script uses jQuery to build an HTML page with content taken from the
  * data defined in data.js.
@@ -18,6 +18,44 @@ export const renderHeroCard = function(hero) {
     // TODO: Generate HTML elements to represent the hero
     // TODO: Return these elements as a string, HTMLElement, or jQuery object
     // Example: return `<div>${hero.name}</div>`;
+
+    return `
+        <div class="card is-4">
+            <div class="card-header" style="background-color: ${hero.backgroundColor}">
+                <p class="card-header-title" style="color:${hero.color}">
+                    ${hero.name}
+                </p>
+            </div>
+            <div class="card-image">
+                <figure class="image is-1by1">
+                    <img src="${hero.img}" alt="Icon for ${hero.name}">
+                </figure>
+            </div>
+            <div class="card-content">
+                <h2 class="">
+                    ${hero.subtitle}
+                </h2>
+                <p class="content">
+                    ${hero.description}
+                </p>
+                <ul class="">
+                    <li class="">
+                        <span class="has-text-weight-bold">Official Identity: </span>
+                        ${hero.first} ${hero.last}
+                    </li>
+                    <li class="">
+                        <span class="has-text-weight-bold">First Seen: </span>
+                        ${hero.firstSeen}
+                    </li>
+                </ul>
+            </div>
+            <div class="card-spacer"></div>
+            <div class="card-footer">
+                <button href="#" class="button card-footer-item">Edit</button>
+            </div>
+        </div>
+    `;
+
 };
 
 
@@ -32,6 +70,71 @@ export const renderHeroEditForm = function(hero) {
     // TODO: Generate HTML elements to represent the hero edit form
     // TODO: Return these elements as a string, HTMLElement, or jQuery object
     // Example: return `<form>${hero.name}</form>`;
+    return `
+        <div class="card is-4">
+            <div class="card-header" style="background-color: ${hero.backgroundColor}">
+                <p class="card-header-title" style="color:${hero.color}">
+                    Edit Hero Form
+                </p>
+            </div>
+            <div class="card-image">
+                <figure class="image is-1by1">
+                    <img src="${hero.img}" alt="Icon for ${hero.name}">
+                </figure>
+            </div>
+            <div class="card-content">
+                
+                <form action="#">
+                    <div class="field">
+                        <label class="label">
+                            Hero Name
+                        </label>
+                        <input type="text" class="input" value="${hero.name}">
+                    </div>
+                    <div class="field">
+                        <label class="label">
+                            First Name
+                        </label>
+                        <input type="text" class="input" value="${hero.first}">
+                    </div>
+                    <div class="field">
+                        <label class="label">
+                            Last Name
+                        </label>
+                        <input type="text" class="input" value="${hero.last}">
+                    </div>
+                    <div class="field">
+                        <label class="label">
+                            Subtitle
+                        </label>
+                        <input type="text" class="input" value="${hero.subtitle}">
+                    </div>
+                    <div class="field">
+                        <label class="label">
+                            Description
+                        </label>
+                        <div class="control">
+                            <textarea cols="30" rows="10" class="textarea">${hero.description}</textarea>
+                       </div>
+                    </div>
+                    <div class="field">
+                        <label class="label">
+                            First Seen
+                        </label>
+                        <input type="date" class="input" value="${hero.firstSeen.toISOString().split('T')[0]}">
+                    </div>
+                    <div class="field is-grouped">
+                        <div class="control">
+                            <button type="submit" class="button is-link">Save</button>
+                        </div>
+                        <div class="control">
+                            <button class="button">Cancel</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    `;
 };
 
 
@@ -45,16 +148,27 @@ export const loadHeroesIntoDOM = function(heroes) {
     // Grab a jQuery reference to the root HTML element
     const $root = $('#root');
 
+    let heroCards = [];
+
     // TODO: Generate the heroes using renderHeroCard()
+    heroes.forEach((hero) => {
+       heroCards.push(renderHeroCard(hero));
+    });
 
     // TODO: Append the hero cards to the $root element
+    heroCards.forEach((card) => {
+        $root.append(document.createRange().createContextualFragment(card));
+    })
 
     // Pick a hero from the list at random
     const randomHero = heroes[Math.floor(Math.random() * heroes.length)];
 
     // TODO: Generate the hero edit form using renderHeroEditForm()
+    let editCard = renderHeroEditForm(randomHero);
+    $root.append(document.createRange().createContextualFragment(editCard));
 
     // TODO: Append the hero edit form to the $root element
+
 };
 
 
@@ -65,3 +179,9 @@ export const loadHeroesIntoDOM = function(heroes) {
 $(function() {
     loadHeroesIntoDOM(heroicData);
 });
+
+
+
+
+
+
